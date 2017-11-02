4aae2ddcb334f22488c9d02d804b4915147f96a9
MihaiToropu <toropumihai@gmail.com>
2017-11-02 19:18:39 +0200

    Revert "Update MiddleSquare"
    This reverts commit 483c9590236d9b079749c7f7841008b33785108b.
    Added: Binomial and Poisson

---
+++ MiddleSquare_LCG_2/src/MS_LCG/Poisson.java
@@ -0,0 +1,37 @@
+package MS_LCG;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Poisson {
+
+    private double _Lambda = 100;
+    private int k = 0;
+    private Generator g;
+    private float p;
+    private float u;
+    private int n;
+    private int iterator = 0;
+    private double L = Math.exp(-_Lambda);
+    private List<Float> myFloatList =
+            new ArrayList<Float>();
+
+    public Poisson(float p, int n, Generator g) {
+        this.p = p;
+        this.n = n;
+        this.g = g;
+        myFloatList = g.NextSeed();
+    }
+
+
+    public int RandomPoisson() {
+        do {
+            k++;
+            u = myFloatList.get(iterator);
+            p = p * u;
+            ++iterator;
+        } while ((p > L) && (k < n));
+        return k - 1;
+
+    }
+}
